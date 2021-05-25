b1457b529480b8cbf6199aa6dd99827ed3218379
Paulo Curado <phcurado@gmail.com>
2021-05-25 16:17:54 +0300

    dump version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule TranslaTable.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/phcurado/transla_table"
-  @version "0.2.2"
+  @version "0.2.3"
 
   def project do
     [
